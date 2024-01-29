2ac329eea543b03e1997e98d7ea1eec86981c26b
Walkways <48722285+Walkways@users.noreply.github.com>
2024-01-29 20:19:32 +0100

    Update __init__.py

--- app/website/__init__.py
+++ app/website/__init__.py
@@ -14,30 +14,30 @@ DB_NAME = "test"
 def create_app():
     app = Flask(__name__)
 
-    #app.config["MYSQL_DATABASE_USER"] = "root"
-    #app.config["MYSQL_DATABASE_PASSWORD"] = "test"
-    #app.config["MYSQL_DATABASE_DB"] = "test"
-    #app.config["MYSQL_DATABASE_HOST"] = "localhost"
-    #app.config["MYSQL_DATABASE_PORT"] = 3306
+    # app.config["MYSQL_DATABASE_USER"] = "root"
+    # app.config["MYSQL_DATABASE_PASSWORD"] = "test"
+    # app.config["MYSQL_DATABASE_DB"] = "test"
+    # app.config["MYSQL_DATABASE_HOST"] = "localhost"
+    # app.config["MYSQL_DATABASE_PORT"] = 3306
     
     app.config['SECRET_KEY'] = "helloworld"
-    #app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    #app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://username:password@00.00.00.00/dbname'
-    ##app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:@localhost/test"
-    #app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:@localhost/" + "database.db"
-    #app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:@localhost/test"    
-    ##app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:@127.0.0.1:3306/test"
-    #app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:@localhost:3306/test"
-    #app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:test@localhost/test"
-    #app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:test@127.0.0.1:3306/test"
+    # app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    # app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://username:password@00.00.00.00/dbname'
+    # app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:@localhost/test"
+    # app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:@localhost/" + "database.db"
+    # app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:@localhost/test"    
+    # app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:@127.0.0.1:3306/test"
+    # app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:@localhost:3306/test"
+    # app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:test@localhost/test"
+    # app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:test@127.0.0.1:3306/test"
 
-    #app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:@127.0.0.1:3306/test3"
+    # app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:@127.0.0.1:3306/test3"
     ip = os.environ.get('BDD_IP', 'valeur_par_defaut')
     mdp = os.environ.get('BDD_MDP', 'valeur_par_defaut')
     chaine = "mysql+pymysql://mansour:" + mdp + "@" + ip + ":3306/test"
-    #ma_variable = os.environ.get('MA_VARIABLE', 'valeur_par_defaut')
+    # ma_variable = os.environ.get('MA_VARIABLE', 'valeur_par_defaut')
     app.config['SQLALCHEMY_DATABASE_URI'] = chaine
-    #app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://mansour:mdp00@00.00.000.000:3306/test"
+    # app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://mansour:mdp00@00.00.000.000:3306/test"
 
     db.init_app(app)
 
