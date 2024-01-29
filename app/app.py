713cba89655679068d52b6c4bca49c6fd6abb4ca
Walkways <48722285+Walkways@users.noreply.github.com>
2024-01-29 20:20:18 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,10 +1,10 @@
 from website import create_app
 
-#if __name__ == "__main__":
+# if __name__ == "__main__":
 #    app = create_app()
 #    app.run(debug=True)
 
-#if __name__ == "__main__":
+# if __name__ == "__main__":
 #    app = create_app()
 #    app.run(debug=True, host='0.0.0.0')
 
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     app = create_app()
     app.run(host="0.0.0.0", port=5000)
 
-#if __name__ == "__main__":
+# if __name__ == "__main__":
 #    app = create_app()
 #    app.run(host="127.0.0.1", port=5000)
 
